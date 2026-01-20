72b25fd75e3ab46af59143be62b094810e76947c
3hosa <130618546+3hosa@users.noreply.github.com>
2026-01-20 11:55:49 -0800

    Add broadcast notification endpoint to URLs

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import LoginView, RegisterView, UserListView, UserUpdateView, UserDeleteView, UserDetailView, KYCSubmissionView, AdminPasswordResetView, NotificationListView, MarkNotificationsReadView
+from .views import LoginView, RegisterView, UserListView, UserUpdateView, UserDeleteView, UserDetailView, KYCSubmissionView, AdminPasswordResetView, NotificationListView, MarkNotificationsReadView, BroadcastNotificationCreateView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -14,4 +14,5 @@ urlpatterns = [
     path('kyc/requests/', KYCSubmissionView.as_view(), name='kyc-submission'),
     path('notifications/', NotificationListView.as_view(), name='notifications-list'),
     path('notifications/mark_all_read/', MarkNotificationsReadView.as_view(), name='notifications-mark-read'),
+    path('notifications/broadcast/', BroadcastNotificationCreateView.as_view(), name='broadcast-notification'),
 ]
