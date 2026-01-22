d79bdef1836d8586c48109721c5802ee3b350aec
3hosa <130618546+3hosa@users.noreply.github.com>
2026-01-22 07:53:25 -0800

    Update settings.py

--- saifi/settings.py
+++ saifi/settings.py
@@ -137,9 +137,10 @@ AUTH_PASSWORD_VALIDATORS = [
 STATIC_URL = 'static/'
 
 # Alzajil (Utility Payment Service) Settings
-ALZAJIL_BASE_URL = 'http://api.s2d-sys.com/api/tp/v1' # Example URL for S2D-Sys
+ALZAJIL_BASE_URL = 'https://alzajilonline.com:8448/api/tp/v1' # Example URL for S2D-Sys
 ALZAJIL_USERNAME = 'alsaifitest'
 ALZAJIL_TOKEN = 'alat@210126' # In many implementations, Password acts as the Security Token
 ALZAJIL_AGENT_USER_ID = 'alsaifitest' # USR parameter, usually same as Username or provided ID
 ALZAJIL_REPORT_USERNAME = 'alsaifitest_rep'
 ALZAJIL_REPORT_PASSWORD = '81497136'
+
